0b9a1748b3334f143e4d7358059719ff5daa32f8
Holger Brandl <holgerbrandl@gmail.com>
2018-06-13 10:39:07 +0200

    increased max heapspace to 2gb

--- deployment/OpenCards.sh
+++ deployment/OpenCards.sh
@@ -9,4 +9,4 @@ then
 fi
 
 # see https://stackoverflow.com/questions/43574426/how-to-resolve-java-lang-noclassdeffounderror-javax-xml-bind-jaxbexception-in-j
-java -Xmx512m -XX:+IgnoreUnrecognizedVMOptions --add-modules=ALL-SYSTEM --add-exports=javafx.graphics/com.sun.javafx.application=ALL-UNNAMED -jar $ocjar
+java -Xmx2g -XX:+IgnoreUnrecognizedVMOptions --add-modules=ALL-SYSTEM --add-exports=javafx.graphics/com.sun.javafx.application=ALL-UNNAMED -jar $ocjar
